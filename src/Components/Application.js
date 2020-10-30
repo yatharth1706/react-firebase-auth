749dc4fca7c07ddfa542117a65b643fbec6c4158
yatharth <yatharthverma070@gmail.com>
2020-10-30 12:39:34 +0530

    Add Tailwind css

--- src/Components/Application.js
+++ src/Components/Application.js
@@ -18,3 +18,5 @@ const Application = () => {
         </Router>
     )
 }
+
+export default Application;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Application from './Components/Application';
+import 'tailwindcss/dist/tailwind.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
   document.getElementById('root')
 );
